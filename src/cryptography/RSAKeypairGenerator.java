1969e9a67bccba9294c7fa1423c434e46cc843c6
Magnus Farstad <magnus.r.farstad@outlook.com>
2022-03-15 15:27:55 +0100

    Added methdos for generating key pair

--- src/cryptography/RSAKeypairGenerator.java
+++
@@ -1,4 +0,0 @@
-package cryptography;
-
-public class RSAKeypairGenerator {
-}
---
+++ src/cryptography/RSAUtil.java
@@ -0,0 +1,26 @@
+package cryptography;
+
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+public class RSAUtil {
+
+    public static PublicKey getPublicKey(String base64PublicKey){
+        PublicKey publicKey = null;
+        try{
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(base64PublicKey.getBytes()));
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            publicKey = keyFactory.generatePublic(keySpec);
+            return publicKey;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return publicKey;
+    }
+}
