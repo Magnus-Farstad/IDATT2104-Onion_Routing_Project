75fcab46a2e673e2959d3fb44ac6e4862f04ba14
Magnus Farstad <magnus.r.farstad@outlook.com>
2022-03-17 12:33:26 +0100

    Updated communication to include encryption and decryption

--- src/Client.java
+++ src/Client.java
@@ -1,33 +1,52 @@
+import cryptography.RSAKeyPairGenerator;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.*;
 import java.net.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
-        final int PORTNR = 1250;
 
+    private final String PUBLIC_KEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCbOoTJNXIUXkWu+lQFceNmAxgL/mxsC5cQGcmy8APaRblNqI9U/aXQ1kHp+Jv2KNKDhUACrHdRVpyCz7XSFNyLPvpOA2DAkhECvhQOGtcGeYIcdUIlHLv3tPIJfZw7WMhmYsWaRm/ITOT06MjUy9QKigxDzrxBF/i4mvR6ff+4VQIDAQAB";
+
+    public static void main(String[] args) throws IOException, IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        final int PORTNR = 1250;
+        final RSAKeyPairGenerator keyPairGenerator = new RSAKeyPairGenerator();
+        keyPairGenerator.initFromStrings();
+
+        /* Bruker en scanner til å lese fra kommandovinduet */
         Scanner leserFraKommandovindu = new Scanner(System.in);
         System.out.print("Oppgi navnet på maskinen der tjenerprogrammet kjører: ");
         String tjenermaskin = leserFraKommandovindu.nextLine();
 
+        /* Setter opp forbindelsen til tjenerprogrammet */
         Socket forbindelse = new Socket(tjenermaskin, PORTNR);
         System.out.println("Nå er forbindelsen opprettet.");
 
+        /* �pner en forbindelse for kommunikasjon med tjenerprogrammet */
         InputStreamReader leseforbindelse = new InputStreamReader(forbindelse.getInputStream());
         BufferedReader leseren = new BufferedReader(leseforbindelse);
         PrintWriter skriveren = new PrintWriter(forbindelse.getOutputStream(), true);
 
+        /* Leser innledning fra tjeneren og skriver den til kommandovinduet */
         String innledning1 = leseren.readLine();
         System.out.println(innledning1);
 
+        /* Leser tekst fra kommandovinduet (brukeren) */
         String enLinje = leserFraKommandovindu.nextLine();
         while (!enLinje.equals("")) {
-            skriveren.println(enLinje);
+            String encryptedMessage = keyPairGenerator.encrypt(enLinje);
+            skriveren.println(encryptedMessage);
             String respons = leseren.readLine();  // mottar respons fra tjeneren
             System.out.println("Fra tjenerprogrammet: " + respons);
             enLinje = leserFraKommandovindu.nextLine();
         }
 
+        /* Lukker forbindelsen */
         leseren.close();
         skriveren.close();
         forbindelse.close();
--- src/cryptography/RSAKeyPairGenerator.java
+++ src/cryptography/RSAKeyPairGenerator.java
@@ -1,17 +1,34 @@
 package cryptography;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.security.*;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
 public class RSAKeyPairGenerator {
 
     private PrivateKey privateKey;
     private PublicKey publicKey;
+    private final String PRIVATE_KEY = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAJs6hMk1chReRa76VAVx42YDGAv+bGwLlxAZybLwA9pFuU2oj1T9pdDWQen4m/Yo0oOFQAKsd1FWnILPtdIU3Is++k4DYMCSEQK+FA4a1wZ5ghx1QiUcu/e08gl9nDtYyGZixZpGb8hM5PToyNTL1AqKDEPOvEEX+Lia9Hp9/7hVAgMBAAECgYAEmiSLrpj2P6FDQcqx6qF6Sccxu9ZNEb7lzE3tjy4eD4kh40h0lZyP53pGzIcbMjSjj5TJP4G+GJpFSpEybEnpjxPIljg42pEJNvpKLxbT86ZCM0WN4qfXL5dA5sWqY+laB59Td+fATnc1Nj0n8CL3/i6PpQwg01M5qoJTPgVZAQJBANmfWXcX88s+XuFB7hFjLGi0euCvMW/dXeETWk1CYQEkMGEG5+ElrGiLFiraO6CIdZ1nOQueD7RQtM5846taKMECQQC2ml+qFhCGzez8HK1cMXEgMNzM3aLhvUmp6fNNhqDSNSYzGy1K6NiJX1CE9yOa525M4nZ1Jh1vPnrws3BOBgCVAkEAmqCUywAj45fPhrJ327bhyQvj+12//MIHgHNlyFuP3WW/UlG71MgV9rpM5+nkUC5lk4/SgqSud+qYbddjVU9cgQJAEbh5gDAT+oERdoXx7Ph/Wfhj9R2tKOsNsweZLPTbtoqh4mPIyXQ/T1WIot64/ddnxN5VUJkaUilmFOXVCD1c4QJBAIaZyp6Q8HIyL7iwClaCAfu8mSt+aB+9s4avPzexFJrvfogunGaU62F6oV78LRsiDbe+6sjG0nGqnWDvkrSDpH0=";
+    private final String PUBLIC_KEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCbOoTJNXIUXkWu+lQFceNmAxgL/mxsC5cQGcmy8APaRblNqI9U/aXQ1kHp+Jv2KNKDhUACrHdRVpyCz7XSFNyLPvpOA2DAkhECvhQOGtcGeYIcdUIlHLv3tPIJfZw7WMhmYsWaRm/ITOT06MjUy9QKigxDzrxBF/i4mvR6ff+4VQIDAQAB";
 
-    public RSAKeyPairGenerator() throws NoSuchAlgorithmException {
+//    public RSAKeyPairGenerator() throws NoSuchAlgorithmException {
+//        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+//        keyGen.initialize(1024);
+//        KeyPair pair = keyGen.generateKeyPair();
+//        this.privateKey = pair.getPrivate();
+//        this.publicKey = pair.getPublic();
+//    }
+
+    public void initKeys() throws NoSuchAlgorithmException {
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
         keyGen.initialize(1024);
         KeyPair pair = keyGen.generateKeyPair();
@@ -19,15 +36,54 @@ public class RSAKeyPairGenerator {
         this.publicKey = pair.getPublic();
     }
 
-    public void writeToFile(String path, byte[] key) throws IOException {
-        File f = new File(path);
-        f.getParentFile().mkdirs();
+    public void initFromStrings(){
+        try{
+            X509EncodedKeySpec keySpecPublic = new X509EncodedKeySpec(decode(PUBLIC_KEY));
+            PKCS8EncodedKeySpec keySpecPrivate = new PKCS8EncodedKeySpec(decode(PRIVATE_KEY));
 
-        FileOutputStream fos = new FileOutputStream(f);
-        fos.write(key);
-        fos.flush();
-        fos.close();
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+
+            publicKey = keyFactory.generatePublic(keySpecPublic);
+            privateKey = keyFactory.generatePrivate(keySpecPrivate);
+        }catch (Exception ignored){}
     }
+
+    public String encrypt(String data) throws BadPaddingException, IllegalBlockSizeException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] encryptedMessage = cipher.doFinal(data.getBytes());
+        return encode(encryptedMessage);
+    }
+
+    private String encode(byte[] data) {
+        return Base64.getEncoder().encodeToString(data);
+    }
+
+    public String decrypt(String encryptedMessage) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decryptedMessage = cipher.doFinal(decode(encryptedMessage));
+        return new String(decryptedMessage, "UTF8");
+    }
+
+    private byte[] decode(String data) {
+        return Base64.getDecoder().decode(data);
+    }
+
+    public void printKeys() {
+        System.out.println("\nPublic key:" + encode(publicKey.getEncoded()));
+        System.out.println("Private key: " + encode(privateKey.getEncoded()));
+    }
+
+//    public void writeToFile(String path, byte[] key) throws IOException {
+//        File f = new File(path);
+//        f.getParentFile().mkdirs();
+//
+//        FileOutputStream fos = new FileOutputStream(f);
+//        fos.write(key);
+//        fos.flush();
+//        fos.close();
+//    }
 
     public PrivateKey getPrivateKey() {
         return privateKey;
@@ -37,11 +93,19 @@ public class RSAKeyPairGenerator {
         return publicKey;
     }
 
-    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException, IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, InvalidKeyException {
         RSAKeyPairGenerator keyPairGenerator = new RSAKeyPairGenerator();
-        keyPairGenerator.writeToFile("RSA/publicKey", keyPairGenerator.getPublicKey().getEncoded());
-        keyPairGenerator.writeToFile("RSA/privateKey", keyPairGenerator.getPrivateKey().getEncoded());
-        System.out.println(Base64.getEncoder().encodeToString(keyPairGenerator.getPublicKey().getEncoded()));
-        System.out.println(Base64.getEncoder().encodeToString(keyPairGenerator.getPrivateKey().getEncoded()));
+        keyPairGenerator.initFromStrings();
+        String message = "Hei på deg!";
+        String encryptedMessage = keyPairGenerator.encrypt(message);
+
+        System.out.println("Encrypted message: " + encryptedMessage);
+        System.out.println("Decrypted message: " + keyPairGenerator.decrypt(encryptedMessage));
+
+        keyPairGenerator.printKeys();
+//        keyPairGenerator.writeToFile("RSA/publicKey", keyPairGenerator.getPublicKey().getEncoded());
+//        keyPairGenerator.writeToFile("RSA/privateKey", keyPairGenerator.getPrivateKey().getEncoded());
+//        System.out.println(Base64.getEncoder().encodeToString(keyPairGenerator.getPublicKey().getEncoded()));
+//        System.out.println(Base64.getEncoder().encodeToString(keyPairGenerator.getPrivateKey().getEncoded()));
     }
 }
