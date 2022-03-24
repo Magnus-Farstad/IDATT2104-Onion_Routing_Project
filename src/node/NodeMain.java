4108bf5a2aa78d69dba6f72cad722df04329a9f2
Magnus Farstad <magnus.r.farstad@outlook.com>
2022-03-24 14:34:48 +0100

    Updated NodeMain

--- src/node/NodeMain.java
+++ src/node/NodeMain.java
@@ -11,6 +11,7 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.PublicKey;
 import java.util.Scanner;
 
 import static API.APIService.apiGETRequest;
@@ -18,22 +19,32 @@ import static API.APIService.apiPOSTNode;
 
 public class NodeMain {
 
+    private final static String PUBLIC_KEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCbOoTJNXIUXkWu+lQFceNmAxgL/mxsC5cQGcmy8APaRblNqI9U/aXQ1kHp+Jv2KNKDhUACrHdRVpyCz7XSFNyLPvpOA2DAkhECvhQOGtcGeYIcdUIlHLv3tPIJfZw7WMhmYsWaRm/ITOT06MjUy9QKigxDzrxBF/i4mvR6ff+4VQIDAQAB";
+
 
     public static void main(String[] args) throws Exception {
-        String publickey = apiGETRequest("http://localhost:8080/getPublicKey");
+        Scanner leserFraKommandovindu = new Scanner(System.in);
+        System.out.println("Skriv inn addressen til serveren");
 
-        EncryptionManager encryptionManager = new EncryptionManager(publickey);
+        String serverAddress = leserFraKommandovindu.nextLine();
+        //String publickey = apiGETRequest("http://" + serverAddress + ":8080/getPublicKey");
+
+        EncryptionManager encryptionManager = new EncryptionManager(PUBLIC_KEY);
         encryptionManager.initFromStrings();
 
         AESencryption aesEncryption = new AESencryption();
         SecretKey aesKey = aesEncryption.generateAESKey();
 
+        String melding = "Hei dette er meg";
+
+        System.out.println("Melding: " + melding);
+        String encrypted = aesEncryption.encrypt(melding, aesKey);
+        System.out.println("Encrypted message: " + encrypted);
+        System.out.println(aesEncryption.decrypt(encrypted, aesKey));
+
         System.out.println("Skriv inn portnummer til noden");
-        Scanner leserFraKommandovindu = new Scanner(System.in);
         String PORTNR = leserFraKommandovindu.nextLine();
 
-        System.out.println("Skriv inn addressen til serveren");
-        String serverAddress = leserFraKommandovindu.nextLine();
 
         String nodeAddress = InetAddress.getLocalHost().getHostAddress();
 
@@ -61,17 +72,18 @@ public class NodeMain {
 
         System.out.println(decryptedData);
 
+        String[] nextNode = decryptedData.split(",");
 
-        /*
-        Socket forbindelse2 = new Socket("localhost", Integer.parseInt(decryptedData));
+        Socket forbindelse2 = new Socket(nextNode[2], Integer.parseInt(nextNode[1]));
         InputStreamReader leseforbindelse2 = new InputStreamReader(forbindelse2.getInputStream());
         BufferedReader leseren2 = new BufferedReader(leseforbindelse2);
         PrintWriter skriveren2 = new PrintWriter(forbindelse2.getOutputStream(), true);
 
 
-         */
-        while (encryptedMessage != null) {
+        String message = leseren2.readLine();
+        while (nextNode[0] != "") {
             System.out.println("sender krypter melding og addresse videre til node 2");
+            skriveren2.println(nextNode[0]);
             //skriveren2.println(encryptedMessage);
             //String respons = leseren2.readLine();  // mottar respons fra tjeneren
             skriveren.println(decryptedData);
