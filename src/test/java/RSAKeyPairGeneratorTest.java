bea9124c18c1d4a2b147ce0a0b729f371563fa29
Magnus Farstad <magnus.r.farstad@outlook.com>
2022-03-25 13:10:41 +0100

    Updated RSAKeyPairGeneratorTest

--- src/test/java/RSAKeyPairGeneratorTest.java
+++ src/test/java/RSAKeyPairGeneratorTest.java
@@ -1,8 +1,9 @@
 import cryptography.AESencryption;
 import cryptography.RSAKeyPairGenerator;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
@@ -15,30 +16,59 @@ import java.security.NoSuchAlgorithmException;
 @DisplayName("RSA Key pair generator test")
 public class RSAKeyPairGeneratorTest {
 
-    @DisplayName("")
-    @Test
-    public void successfullyGenerateKeys() {
+    @DisplayName("Tests for encryption")
+    @Nested
+    public class Encrypt {
 
-    }
+        @DisplayName("Successfully encrypt a string with RSA")
+        @Test
+        public void successfullyEncryptString() {
 
-    @DisplayName("Test")
-    @Test
-    public void testEncryptDecryptRSA() throws NoSuchAlgorithmException, IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, InvalidKeyException, UnsupportedEncodingException {
+            RSAKeyPairGenerator rsaKeyPairGenerator = null;
+            String messageToBeEncrypted = "Hello, I am not encrypted!";
+            String encryptedMessage = null;
+            try {
+                rsaKeyPairGenerator = new RSAKeyPairGenerator();
+                rsaKeyPairGenerator.initFromStrings("MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCbOoTJNXIUXkWu+lQFceNmAxgL/mxsC5cQGcmy8APaRblNqI9U/aXQ1kHp+Jv2KNKDhUACrHdRVpyCz7XSFNyLPvpOA2DAkhECvhQOGtcGeYIcdUIlHLv3tPIJfZw7WMhmYsWaRm/ITOT06MjUy9QKigxDzrxBF/i4mvR6ff+4VQIDAQAB", "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAJs6hMk1chReRa76VAVx42YDGAv+bGwLlxAZybLwA9pFuU2oj1T9pdDWQen4m/Yo0oOFQAKsd1FWnILPtdIU3Is++k4DYMCSEQK+FA4a1wZ5ghx1QiUcu/e08gl9nDtYyGZixZpGb8hM5PToyNTL1AqKDEPOvEEX+Lia9Hp9/7hVAgMBAAECgYAEmiSLrpj2P6FDQcqx6qF6Sccxu9ZNEb7lzE3tjy4eD4kh40h0lZyP53pGzIcbMjSjj5TJP4G+GJpFSpEybEnpjxPIljg42pEJNvpKLxbT86ZCM0WN4qfXL5dA5sWqY+laB59Td+fATnc1Nj0n8CL3/i6PpQwg01M5qoJTPgVZAQJBANmfWXcX88s+XuFB7hFjLGi0euCvMW/dXeETWk1CYQEkMGEG5+ElrGiLFiraO6CIdZ1nOQueD7RQtM5846taKMECQQC2ml+qFhCGzez8HK1cMXEgMNzM3aLhvUmp6fNNhqDSNSYzGy1K6NiJX1CE9yOa525M4nZ1Jh1vPnrws3BOBgCVAkEAmqCUywAj45fPhrJ327bhyQvj+12//MIHgHNlyFuP3WW/UlG71MgV9rpM5+nkUC5lk4/SgqSud+qYbddjVU9cgQJAEbh5gDAT+oERdoXx7Ph/Wfhj9R2tKOsNsweZLPTbtoqh4mPIyXQ/T1WIot64/ddnxN5VUJkaUilmFOXVCD1c4QJBAIaZyp6Q8HIyL7iwClaCAfu8mSt+aB+9s4avPzexFJrvfogunGaU62F6oV78LRsiDbe+6sjG0nGqnWDvkrSDpH0=");
 
-        RSAKeyPairGenerator rsaKeyPairGenerator = new RSAKeyPairGenerator();
+                encryptedMessage = rsaKeyPairGenerator.encrypt(messageToBeEncrypted);
 
-        rsaKeyPairGenerator.initFromStrings("MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCbOoTJNXIUXkWu+lQFceNmAxgL/mxsC5cQGcmy8APaRblNqI9U/aXQ1kHp+Jv2KNKDhUACrHdRVpyCz7XSFNyLPvpOA2DAkhECvhQOGtcGeYIcdUIlHLv3tPIJfZw7WMhmYsWaRm/ITOT06MjUy9QKigxDzrxBF/i4mvR6ff+4VQIDAQAB", "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAJs6hMk1chReRa76VAVx42YDGAv+bGwLlxAZybLwA9pFuU2oj1T9pdDWQen4m/Yo0oOFQAKsd1FWnILPtdIU3Is++k4DYMCSEQK+FA4a1wZ5ghx1QiUcu/e08gl9nDtYyGZixZpGb8hM5PToyNTL1AqKDEPOvEEX+Lia9Hp9/7hVAgMBAAECgYAEmiSLrpj2P6FDQcqx6qF6Sccxu9ZNEb7lzE3tjy4eD4kh40h0lZyP53pGzIcbMjSjj5TJP4G+GJpFSpEybEnpjxPIljg42pEJNvpKLxbT86ZCM0WN4qfXL5dA5sWqY+laB59Td+fATnc1Nj0n8CL3/i6PpQwg01M5qoJTPgVZAQJBANmfWXcX88s+XuFB7hFjLGi0euCvMW/dXeETWk1CYQEkMGEG5+ElrGiLFiraO6CIdZ1nOQueD7RQtM5846taKMECQQC2ml+qFhCGzez8HK1cMXEgMNzM3aLhvUmp6fNNhqDSNSYzGy1K6NiJX1CE9yOa525M4nZ1Jh1vPnrws3BOBgCVAkEAmqCUywAj45fPhrJ327bhyQvj+12//MIHgHNlyFuP3WW/UlG71MgV9rpM5+nkUC5lk4/SgqSud+qYbddjVU9cgQJAEbh5gDAT+oERdoXx7Ph/Wfhj9R2tKOsNsweZLPTbtoqh4mPIyXQ/T1WIot64/ddnxN5VUJkaUilmFOXVCD1c4QJBAIaZyp6Q8HIyL7iwClaCAfu8mSt+aB+9s4avPzexFJrvfogunGaU62F6oV78LRsiDbe+6sjG0nGqnWDvkrSDpH0=");
+            } catch (NoSuchAlgorithmException | BadPaddingException | IllegalBlockSizeException | InvalidKeyException | NoSuchPaddingException exception) {
+                System.out.println(exception.getMessage());
+            } finally {
+                Assertions.assertNotEquals(messageToBeEncrypted, encryptedMessage);
+            }
+        }
 
-        AESencryption aesEncryption = new AESencryption();
+        @DisplayName("Successfully encrypt AES key with RSA")
+        @Test
+        public void successfullyEncryptAesKey() {
 
-        SecretKey aesKey = aesEncryption.generateAESKey();
+            RSAKeyPairGenerator rsaKeyPairGenerator = null;
 
-        String aesKeyString = aesEncryption.convertSecretKeyToString(aesKey);
-        String encryptedAES = rsaKeyPairGenerator.encrypt(aesKeyString);
+            AESencryption aesEncryption = new AESencryption();
+            SecretKey aesKey = null;
+            String aesKeyString = "Empty";
+            String encryptedAES = null;
+            String decryptedAES = null;
+            try {
+                rsaKeyPairGenerator = new RSAKeyPairGenerator();
+                rsaKeyPairGenerator.initFromStrings("MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCbOoTJNXIUXkWu+lQFceNmAxgL/mxsC5cQGcmy8APaRblNqI9U/aXQ1kHp+Jv2KNKDhUACrHdRVpyCz7XSFNyLPvpOA2DAkhECvhQOGtcGeYIcdUIlHLv3tPIJfZw7WMhmYsWaRm/ITOT06MjUy9QKigxDzrxBF/i4mvR6ff+4VQIDAQAB", "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAJs6hMk1chReRa76VAVx42YDGAv+bGwLlxAZybLwA9pFuU2oj1T9pdDWQen4m/Yo0oOFQAKsd1FWnILPtdIU3Is++k4DYMCSEQK+FA4a1wZ5ghx1QiUcu/e08gl9nDtYyGZixZpGb8hM5PToyNTL1AqKDEPOvEEX+Lia9Hp9/7hVAgMBAAECgYAEmiSLrpj2P6FDQcqx6qF6Sccxu9ZNEb7lzE3tjy4eD4kh40h0lZyP53pGzIcbMjSjj5TJP4G+GJpFSpEybEnpjxPIljg42pEJNvpKLxbT86ZCM0WN4qfXL5dA5sWqY+laB59Td+fATnc1Nj0n8CL3/i6PpQwg01M5qoJTPgVZAQJBANmfWXcX88s+XuFB7hFjLGi0euCvMW/dXeETWk1CYQEkMGEG5+ElrGiLFiraO6CIdZ1nOQueD7RQtM5846taKMECQQC2ml+qFhCGzez8HK1cMXEgMNzM3aLhvUmp6fNNhqDSNSYzGy1K6NiJX1CE9yOa525M4nZ1Jh1vPnrws3BOBgCVAkEAmqCUywAj45fPhrJ327bhyQvj+12//MIHgHNlyFuP3WW/UlG71MgV9rpM5+nkUC5lk4/SgqSud+qYbddjVU9cgQJAEbh5gDAT+oERdoXx7Ph/Wfhj9R2tKOsNsweZLPTbtoqh4mPIyXQ/T1WIot64/ddnxN5VUJkaUilmFOXVCD1c4QJBAIaZyp6Q8HIyL7iwClaCAfu8mSt+aB+9s4avPzexFJrvfogunGaU62F6oV78LRsiDbe+6sjG0nGqnWDvkrSDpH0=");
 
-        String decryptedAES = rsaKeyPairGenerator.decrypt(encryptedAES);
+                aesKey = aesEncryption.generateAESKey();
+                aesKeyString = aesEncryption.convertSecretKeyToString(aesKey);
 
-        Assertions.assertNotEquals(aesKeyString, encryptedAES);
-        Assertions.assertEquals(aesKeyString, decryptedAES);
+                encryptedAES = rsaKeyPairGenerator.encrypt(aesKeyString);
+                decryptedAES = rsaKeyPairGenerator.decrypt(encryptedAES);
+            } catch (NoSuchAlgorithmException | BadPaddingException | IllegalBlockSizeException | InvalidKeyException | NoSuchPaddingException | UnsupportedEncodingException exception) {
+                System.out.println(exception.getMessage());
+            } finally {
+                Assertions.assertEquals(aesKeyString, decryptedAES);
+            }
+
+
+            Assertions.assertNotEquals(aesKeyString, encryptedAES);
+            Assertions.assertEquals(aesKeyString, decryptedAES);
+        }
     }
 }
