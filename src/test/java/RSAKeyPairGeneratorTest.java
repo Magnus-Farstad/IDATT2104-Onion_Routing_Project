635e8228dd3d314a0431c2030efee31a36ad93b7
Magnus Farstad <magnus.r.farstad@outlook.com>
2022-03-25 11:34:35 +0100

    Updated RSAKeyPairGeneratorTest

--- src/test/java/RSAKeyPairGeneratorTest.java
+++ src/test/java/RSAKeyPairGeneratorTest.java
@@ -1,2 +1,12 @@
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("RSA Key pair generator test")
 public class RSAKeyPairGeneratorTest {
+
+    @DisplayName("")
+    @Test
+    public void successfullyGenerateKeys() {
+
+    }
 }
