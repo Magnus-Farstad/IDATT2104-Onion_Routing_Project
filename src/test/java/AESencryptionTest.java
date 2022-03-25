dd37f971766db36f16bf8988215fed5316b3f641
Simen Klemp <simenk2312@gmail.com>
2022-03-25 13:15:31 +0100

    Revision in class structure in AESencryptionTest class

--- src/test/java/AESencryptionTest.java
+++ src/test/java/AESencryptionTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.Nested;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
@@ -14,41 +14,85 @@ import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
-class AESencryptionTest {
 
-    @DisplayName("Successfully generate keys")
-    @Test
-    public void successfullyGenerateKeys() throws NoSuchAlgorithmException {
-        AESencryption aeSencryption = new AESencryption();
+public class AESencryptionTest {
+    @Nested
+    class generateKeysTest {
+        @DisplayName("Successfully generate keys positive")
+        @Test
+        public void successfullyGenerateKeysPositive() throws NoSuchAlgorithmException {
+            try {
+                AESencryption aeSencryption = new AESencryption();
 
-        String secretAESKEY = aeSencryption.convertSecretKeyToString(aeSencryption.generateAESKey());
+                String secretAESKEY = aeSencryption.convertSecretKeyToString(aeSencryption.generateAESKey());
 
-        Assertions.assertNotNull(secretAESKEY);
-
-    }
-
-
-    @DisplayName("Encrypt and decrypt with AES")
-    @Test
-    public void encryptDecryptAES(){
-
-        try{
-            AESencryption aesEncryption = new AESencryption();
-
-            SecretKey secretKey = aesEncryption.generateAESKey();
-
-            String message = "This is a message";
-
-            String encryptedMessage = aesEncryption.encrypt(message, secretKey);
-
-            String decryptedMessage = aesEncryption.decrypt(encryptedMessage, secretKey);
-
-            Assertions.assertNotEquals(message, encryptedMessage);
-            Assertions.assertEquals(message, decryptedMessage);
-
-        } catch (Exception exception) {
-            System.out.println(exception.getMessage());
+                Assertions.assertNotNull(secretAESKEY);
+            } catch (Exception exception) {
+                System.out.println(exception.getMessage());
+            }
         }
 
+        @DisplayName("Successfully generate keys negative")
+        @Test
+        public void successfullyGenerateKeysNegative() throws NoSuchAlgorithmException {
+            try {
+                AESencryption aeSencryption = new AESencryption();
+
+                String secretAESKEY = aeSencryption.convertSecretKeyToString(aeSencryption.generateAESKey());
+
+                Assertions.assertNull(secretAESKEY);
+            } catch (Exception exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
+
+    @Nested
+    class encryptDecryptAES {
+        @DisplayName("Encrypt and decrypt with AES Positive")
+        @Test
+        public void encryptDecryptAESPositive() {
+
+            try {
+                AESencryption aesEncryption = new AESencryption();
+
+                SecretKey secretKey = aesEncryption.generateAESKey();
+
+                String message = "This is a message";
+
+                String encryptedMessage = aesEncryption.encrypt(message, secretKey);
+
+                String decryptedMessage = aesEncryption.decrypt(encryptedMessage, secretKey);
+
+                Assertions.assertNotEquals(message, encryptedMessage);
+                Assertions.assertEquals(message, decryptedMessage);
+
+            } catch (Exception exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+
+        @DisplayName("Encrypt and decrypt with AES Negative")
+        @Test
+        public void encryptDecryptAESNegative() {
+
+            try {
+                AESencryption aesEncryption = new AESencryption();
+
+                SecretKey secretKey = aesEncryption.generateAESKey();
+
+                String message = "This is a message";
+
+                String encryptedMessage = aesEncryption.encrypt(message, secretKey);
+
+                String decryptedMessage = aesEncryption.decrypt(encryptedMessage, secretKey);
+
+                Assertions.assertNotEquals(message, encryptedMessage);
+                Assertions.assertEquals(message, decryptedMessage);
+
+            } catch (Exception exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
     }
 }
